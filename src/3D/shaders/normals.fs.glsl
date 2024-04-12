d76de9fb8606214b165e13a6fe47439ee3ddc6ad
Mélodie KOUY <melodiekouy@gmail.com>
2024-04-12 08:43:23 +0200

    couleur abeille

--- src/3D/shaders/normals.fs.glsl
+++ src/3D/shaders/normals.fs.glsl
@@ -7,5 +7,9 @@ in vec2 vTexCoords; // Coordonnées de texture du sommet
 out vec4 fFragColor;
 
 void main() {
-  fFragColor = vec4(normalize(vNormal_vs), 1.);
+  // TODO pour le debug
+  // fFragColor = vec4(normalize(vNormal_vs), 1.);
+  // TODO renommer le fichier en Bee / abeille
+  fFragColor = vec4(0.9, 0.8, 0., 1.); 
+  fFragColor = fFragColor * mod(floor(2.5 * vPosition_vs.x), 2.0);
 };
