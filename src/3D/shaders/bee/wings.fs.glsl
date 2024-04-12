293af80d9a9d967c85dccb48e4ef9be39d9e9905
Mélodie KOUY <melodiekouy@gmail.com>
2024-04-12 10:03:37 +0200

    + de bleu

--- src/3D/shaders/bee/wings.fs.glsl
+++ src/3D/shaders/bee/wings.fs.glsl
@@ -7,10 +7,10 @@ in vec3 vPosition_vs; // Position du sommet transformée dans l'espace View (vs)
 out vec4 fFragColor;
 
 void main() {
-  // fFragColor = vec4(1., 1., 1., 1.); 
+  fFragColor = vec4(1., 1., 1., 1.); 
 
   float alpha = 1.; // intensité du centre
-  float beta = 2.;  // vitesse de dégradé
+  float beta = 0.5;  // vitesse de dégradé
   float a = alpha * exp(-beta * distance(vec2(0., 0.), vPosition_vs.xy) * distance(vec2(0., 0.), vPosition_vs.xy));
 
   fFragColor = vec4(0., 0.8, 1., 0.3) * a;
