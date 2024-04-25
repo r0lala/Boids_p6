24e54a55d73c92eed8b92da0c6805e1b547b2068
Mélo <melodiekouy@gmail.com>
2024-04-25 10:36:03 +0200

    ajout type

--- src/3D/shaders/light/pointLight.fs.glsl
+++ src/3D/shaders/light/pointLight.fs.glsl
@@ -10,18 +10,18 @@ uniform float uShininess;
 uniform vec3 uLightPos_vs;
 uniform vec3 uLightIntensity;
 
-vec3 blinnPhong(){
-    w_i = normalize(uLightPos_vs - vPosition_vs);
-    w_0 = normalize(-vPosition_vs); // pointe vers la caméra
+vec3 blinnPhong(vec3 uLightPos_vs, vec3 vPosition_vs, vec3 uLightIntensity, vec3 uKd, vec3 vNormal_vs, vec3 uKs, float uShininess){
+    vec3 w_i = normalize(uLightPos_vs - vPosition_vs);
+    vec3 w_0 = normalize(-vPosition_vs); // pointe vers la caméra
 
-    return (uLightIntensity / (distance(uLightPos_vs) * distance(uLightPos_vs))) 
+    return (uLightIntensity / (distance(uLightPos_vs, vPosition_vs) * distance(uLightPos_vs, vPosition_vs))) 
     * (uKd * dot(normalize(uLightPos_vs), vNormal_vs)) 
     + uKs * (pow(dot((w_0 + w_i) / 2., vNormal_vs), uShininess));
 }
 
 void main(){
-    fFragColor = blinnPhong();
-}
+    fFragColor = blinnPhong(uLightPos_vs, vPosition_vs, uLightIntensity, uKd, vNormal_vs, uKs, uShininess);
+};
 
 //  variables uniformes vec3 uLightPos_vs 
 //  => nous allons travailler dans le view space 
